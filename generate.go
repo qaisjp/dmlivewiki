d9b655b2365d53b853ffda816f21471c79fb20e5
Qais Patankar <qaisjp@gmail.com>
2015-09-01 23:32:42 +0100

    Reorganise 'generate' files

--- generate.go
+++ generate.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
-	"github.com/codegangsta/cli"
-	"io/ioutil"
+	"os"
 	"os/exec"
-	"path"
 	"strconv"
 	"strings"
-	"text/template"
 	"time"
 )
+
+type Tour struct {
+	Name   string
+	Tracks map[string]struct{}
+}
 
 type AlbumData struct {
 	Artist   string
@@ -29,176 +33,33 @@ type TrackData struct {
 	Index                    int
 }
 
-func generateInformation(c *cli.Context) {
-	fileInfo, filepath := checkFilepathArgument(c)
-	if fileInfo == nil {
-		return
-	}
-
-	tourName := c.String("tour")
-	if tourName == "" {
-		cli.ShowSubcommandHelp(c)
-		return
-	}
-
-	mode := "batch"
-	if c.GlobalBool("single") {
-		mode = "single"
-	}
-
-	tourfile := c.String("tour-file")
-	if tourfile != "" {
-		fileInfo, tourfileClean := getFileOfType(tourfile, false, "tour-file")
-		if fileInfo == nil {
-			return
-		}
-		tourfile = tourfileClean
-		fmt.Println("Processing tours from:", tourfile)
-	}
-
-	fmt.Println("The current tour is:", tourName)
-	fmt.Printf("The following filepath (%s mode) will be processed: %s\n", mode, filepath)
-	notifyDeleteMode(c)
-
-	if !shouldContinue(c) {
-		return
-	}
-
-	tour := new(Tour)
-	tour.Name = tourName
-	if tourfile != "" { // tourFile is only for reading "alternate vocalists" into tracks map
-		if err := getTourFromTourFile(tourfile, tour); err != nil {
-			fmt.Println("[Error]", err)
-			if !shouldContinue(c) {
-				return
-			}
-		}
-	}
-
-	// Stupid windows
-	informationTemplate = strings.Replace(informationTemplate, "\n", "\r\n", -1)
-
-	if mode == "single" {
-		generateFile(filepath, fileInfo.Name(), *tour, c.GlobalBool("delete"))
-		return
-	}
-
-	files, _ := ioutil.ReadDir(filepath)
-	for _, file := range files {
-		if file.IsDir() {
-			name := file.Name()
-			generateFile(path.Join(filepath, name), name, *tour, c.GlobalBool("delete"))
-		}
-	}
-}
-
-func generateFile(filepath string, name string, tour Tour, deleteMode bool) {
-	outputFilename := path.Join(filepath, name+".txt")
-	if deleteMode {
-		removeFile(outputFilename)
-		return
-	}
-
-	album := new(AlbumData)
-	album.Tour = tour.Name
-
-	var duration int64 = 0 // duration incrementer for the album
-
-	useCDNames := false
-	folders := make([]string, 0)
-	extraFolders := make([]string, 0)
-	files := make([]string, 0)
-
-	directoryContents, _ := ioutil.ReadDir(filepath)
-	for _, fileinfo := range directoryContents {
-		filename := fileinfo.Name()
-		isDir := fileinfo.IsDir()
-		if isDir {
-			if strings.HasPrefix(filename, "CD") {
-				folders = append(folders, filename)
-				useCDNames = true
-			} else {
-				extraFolders = append(extraFolders, filename)
-			}
-		} else if (path.Ext(filename) == ".flac") && !isDir {
-			files = append(files, filename)
-		}
-	}
-
-	iterating := files
-	if useCDNames {
-
-		if len(files) > 0 {
-			// Contains extra files not in a specific CD
-			// Do something!
-			fmt.Println("Warning! Files outside CD folders in", filepath)
-		}
-
-		files := make([]string, 0)
-		subfolders := make([]string, 0)
-		for _, dirName := range folders {
-			subdirectory, _ := ioutil.ReadDir(path.Join(filepath, dirName))
-			for _, fileinfo := range subdirectory {
-				subdirPath := path.Join(dirName, fileinfo.Name())
-				if fileinfo.IsDir() {
-					subfolders = append(subfolders, subdirPath)
-				} else {
-					files = append(files, subdirPath)
-				}
-			}
-		}
-
-		if len(subfolders) > 0 {
-			fmt.Printf("Skipping! Filepath has depth=3 folders (%s)\n", filepath)
-			return
-		}
-
-		iterating = files // set it to the new files
-		// this means old files won't be iterated
-	}
-
-	if len(extraFolders) > 0 {
-		// Contains extra folders, do something!
-		// There's probably a folder like "Bonus"
-		fmt.Println("Warning! Extra non CD folders inside", filepath)
-	}
-
-	for _, file := range iterating {
-		track := getTagsFromFile(path.Join(filepath, file), album, &duration)
-
-		if tour.Tracks != nil {
-			_, containsAlternateLeadVocalist := tour.Tracks[track.Title]
-			track.HasAlternateLeadVocalist = containsAlternateLeadVocalist
-		}
-
-		if useCDNames {
-			track.Prefix = strings.TrimPrefix(path.Dir(file), "CD") + "."
-		}
-
-		// Finally, add the new track to the album
-		album.Tracks = append(album.Tracks, track)
-	}
-
-	if len(album.Tracks) == 0 {
-		fmt.Println("Could not create album - aborting creation of", outputFilename)
-		return
-	}
-
-	format := "4:05" // minute:0second
-	if duration >= 3600 {
-		format = "15:04:05" // duration is longer than an hour
-	}
-	album.Duration = time.Unix(duration, 0).Format(format)
-
-	funcMap := template.FuncMap{"wikiescape": wikiescape}
-	t := template.Must(template.New("generate").Funcs(funcMap).Parse(informationTemplate))
-
-	infoFile := createFile(outputFilename)
-	defer infoFile.Close()
-	err := t.Execute(infoFile, album)
+func getTourFromTourFile(filepath string, tour *Tour) error {
+	file, err := os.Open(filepath)
+	defer file.Close()
 	if err != nil {
-		panic(err)
+		return errors.New("Could not open Tourfile (" + err.Error() + ")")
 	}
+
+	reader := bufio.NewReader(file)
+	scanner := bufio.NewScanner(reader)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		prefix := tour.Name + ":"
+		if !strings.HasPrefix(line, prefix) {
+			continue
+		}
+
+		list := strings.TrimSpace(strings.TrimPrefix(line, prefix))
+		tracks := strings.Split(list, ",")
+		tour.Tracks = make(map[string]struct{})
+
+		for _, track := range tracks {
+			tour.Tracks[strings.TrimSpace(track)] = struct{}{}
+		}
+		return nil
+	}
+	return errors.New("Tourfile does not contain tour")
 }
 
 // tags: http://age.hobba.nl/audio/tag_frame_reference.html
