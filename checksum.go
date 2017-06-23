qaisjp/dmlivewiki

commit 6ed300a4770b959bb746ce0b5d00e4b94084a075

fix compilation and linux checksum

checksum.go
@@ -4,12 +4,13 @@ import (
 	"bytes"
 	"crypto/md5"
 	"fmt"
-	"github.com/codegangsta/cli"
 	"io/ioutil"
 	"os"
 	"os/exec"
 	fpath "path/filepath"
 	"strings"
+
+	"gopkg.in/urfave/cli.v1"
 )
 
 func performChecksum(c *cli.Context) {
@@ -72,6 +73,7 @@ func checksumProcessPath(directory string, name string, deleteMode bool, working
 		// because we're going to dump the entire
 		// pool in the command later
 		"--show-md5sum",
+		"--no-filename",
 	}
 
 	// This walks through every file in the folder
@@ -118,9 +120,9 @@ func checksumProcessPath(directory string, name string, deleteMode bool, working
 	)
 
 	// If the pool contains atleast one **filename**
-	// the first item in the pool is actually just a flag!
-	if len(ffpPool) > 1 {
-		cmd := exec.Command(fpath.Join(workingDirectory, "metaflac"), ffpPool[:]...)
+	// the first two items in the pool is actually just a flag!
+	if len(ffpPool) > 2 {
+		cmd := exec.Command("metaflac", ffpPool[:]...)
 		cmd.Dir = directory
 
 		data, err := cmd.Output()
@@ -131,6 +133,17 @@ func checksumProcessPath(directory string, name string, deleteMode bool, working
 			}
 			panic(err)
 		}
+
+		hashes := strings.Split(string(data), "\n")
+		for i, hash := range hashes {
+			if (i == len(hashes)-1) && (hash == "") {
+				// Last line is an empty line
+				continue
+			}
+
+			hashes[i] = fmt.Sprintf("%s:%s", fpath.Base(ffpPool[i+2]), hash)
+		}
+		data = []byte(strings.Join(hashes, "\n"))
 
 		// The md5 buffer doesn't contain our ffp file, so let's write that to the buffer
 		md5Buffer.WriteString(checksumFormatMD5(md5.Sum(data), name+".ffp"))

generate_generators.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
-	"github.com/codegangsta/cli"
 	"io/ioutil"
 	"path"
 	"strings"
 	"text/template"
 	"time"
+
+	"gopkg.in/urfave/cli.v1"
 )
 
 func generateInformation(c *cli.Context) {

util.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"github.com/codegangsta/cli"
 	"io/ioutil"
 	"net/url"
 	"os"
@@ -10,6 +9,8 @@ import (
 	fpath "path/filepath"
 	"strings"
 	"time"
+
+	"gopkg.in/urfave/cli.v1"
 )
 
 func wikiescape(s string) string {
@@ -106,7 +107,6 @@ func getFileErrorReason(err error) string {
 		return err.Error()
 	}
 }
-
 func checkFilepathArgument(c *cli.Context) (os.FileInfo, string) {
 	if len(c.Args()) != 1 {
 		cli.ShowSubcommandHelp(c)

verify.go
@@ -5,12 +5,13 @@ import (
 	"bytes"
 	"crypto/md5"
 	"fmt"
-	"github.com/codegangsta/cli"
 	"io/ioutil"
 	"os"
 	"os/exec"
 	fpath "path/filepath"
 	"strings"
+
+	"gopkg.in/urfave/cli.v1"
 )
 
 var tick, cross string = `ok`, `bad`
@@ -97,7 +98,7 @@ func verifyProcessPath(directory string, name string, workingDirectory string) {
 		fmt.Println(tick)
 		return
 	}
-	
+
 	md5out, ffpOut := cross, cross
 	if md5Success {
 		md5out = tick
@@ -196,7 +197,7 @@ func verifyFFP(ffpFilename string, directory string, workingDirectory string) (s
 		files[:]...,
 	)
 
-	cmd := exec.Command(fpath.Join(workingDirectory, "metaflac"), cmdArgs[:]...)
+	cmd := exec.Command("metaflac", cmdArgs[:]...)
 	cmd.Dir = directory
 	cmd.Stderr = &cmdStderr
 	cmd.Stdout = &cmdStdout

wiki.go
@@ -3,8 +3,6 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"github.com/codegangsta/cli"
-	"github.com/inhies/go-bytesize" // Do we really need this?
 	"io/ioutil"
 	"net/url"
 	"os"
@@ -15,6 +13,9 @@ import (
 	"strconv"
 	"strings"
 	"text/template"
+
+	"github.com/inhies/go-bytesize" // Do we really need this?
+	"gopkg.in/urfave/cli.v1"
 )
 
 type WikiTrackData struct {

commit 372c39aba4103d493418fa4a1225d93b42a025ce

fix ffp inconsistency introduced in 6ed300a4770b959bb746ce0b5d00e4b94084a075

checksum.go
@@ -97,7 +97,7 @@ func checksumProcessPath(directory string, name string, deleteMode bool, working
 
 			name := strings.TrimPrefix(
 				path,
-				directory+"\\",
+				directory+string(os.PathSeparator),
 			)
 
 			if fpath.Ext(path) == ".flac" {
@@ -141,7 +141,7 @@ func checksumProcessPath(directory string, name string, deleteMode bool, working
 				continue
 			}
 
-			hashes[i] = fmt.Sprintf("%s:%s", fpath.Base(ffpPool[i+2]), hash)
+			hashes[i] = fmt.Sprintf("%s:%s", strings.TrimPrefix(ffpPool[i+2], directory), hash)
 		}
 		data = []byte(strings.Join(hashes, "\r\n"))
 